ca64b1892bded6f7718ede2115c77204709dfd39
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2023-04-26 08:54:56 +0200

    two sum

---
+++ solutions/1.two-sum.ts
@@ -0,0 +1,15 @@
+const twoSum = (nums: number[], target: number): number[] => {
+    const map = new Map<number, number>();
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement= target - nums[i];
+
+        if (map.has(complement)) {
+            return [map.get(complement)! , i];
+        }
+
+        map.set(nums[i], i);
+    }
+
+    return [];
+};
